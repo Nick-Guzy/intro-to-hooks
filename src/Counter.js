aeed8e75a434e3a2437f5728ee1061cc98f49ba1
Nick-Guzy <nick.guzy76@gmail.com>
2023-04-28 15:52:28 -0700

    Hooks

---
+++ src/Counter.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  const [counter, setCounter] = useState(0);
+  const [hidden, setHidden] = useState(false);
+
+  return(
+    <React.Fragment>
+      {hidden ? <h1>Count Hidden</h1> : <h1> {counter}</h1>}
+      <button onClick={() => setHidden(!hidden)}>Hide/Show</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>Count!</button>
+    </React.Fragment>
+  )
+}
+
+export default Counter;
