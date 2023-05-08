7951ec980ed073a21c3a36e4b1d98ee45bdbe6a3
Nick-Guzy <nick.guzy76@gmail.com>
2023-05-08 14:30:49 -0700

    Set up hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Counter from './Counter';
+import Timer from './Timer';
 
 function App() {
   return (
-   <div className="App">
-    <Counter />
-   </div>
+    <div className="App">
+      <Counter />
+      <Timer />
+    </div>
   );
 }
 
